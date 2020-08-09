9eff2d839bf2d598cbab27b7d264ed924c3d71d4
Angélica M. Vega Aponte <31680125+angelicavega@users.noreply.github.com>
2020-08-09 14:33:18 -0400

    Add files via upload

---
+++ compare_names.py
@@ -0,0 +1,26 @@
+
+
+#another solution
+
+def comparenames(inputname, yourname, othername1, othername2, othername3):
+    if (inputname == yourname):
+        print('this is a nice name')
+    elif (inputname == othername1):
+        print('you are famous!')
+    elif (inputname == othername2):
+        print('this is a bad name')
+    else:
+        print('I wish I could meet you!')
+
+inputname = input('What is your name?')
+
+yourname = 'Angelica'
+
+othername1 = 'Peter Pan'
+
+othername2 = 'Trump'
+
+othername3 = 'Marie Curie'
+
+comparenames(inputname, yourname, othername1, othername2, othername3)
+    
