8225fab26631ed00668ed2a93b2eacccba1c945f
Angélica M. Vega Aponte <31680125+angelicavega@users.noreply.github.com>
2020-08-09 15:06:19 -0400

    Add files via upload

---
+++ password_creator.py
@@ -0,0 +1,36 @@
+#Here is a simple python password function
+lengthGood = False
+charGood = True
+symbolGood = False
+listGood = False
+passwords = ["linda!@hottie","lolap@$8","chol@12"]
+
+while True:
+    pas = input('Input Password: ')
+    if pas not in passwords:
+        listGood = True
+    else:
+        pass
+    if len(pas) > 8:
+        lengthGood = True
+    for char in pas:
+        if char.isalpha() or char.isdigit() or char in "[!#$%@*]":
+            pass
+        else:
+            charGood = False
+        if char in "[!#$%@*]":
+            symbolGood = True
+        else:
+            pass
+
+    if lengthGood and charGood and symbolGood and listGood:
+        break
+    else:
+        if not lengthGood:
+            print("length bad")
+        if not charGood:
+            print("bad characters")
+        if not symbolGood:
+            print("not enough symbols")
+        if not listGood:
+            print("passwork taken")
