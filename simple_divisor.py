69d953691eb4d39ac4cea4e08731b01451f3d09c
Angélica M. Vega Aponte <31680125+angelicavega@users.noreply.github.com>
2020-08-09 14:46:50 -0400

    Add files via upload

---
+++ simple_divisor.py
@@ -0,0 +1,17 @@
+def divisor2_3(x):
+    if (x%2) == 0:
+        if (x%3) == 0:
+           print("it can be divided by both 2 and 3")
+        else:
+            print("it can only be divided by 2 but not 3")
+
+    else:
+        if (x%3) == 0:
+            print("it can only be divided by 3 but not 2")
+        else:
+            print("it can not be divided by neither 2 or 3")
+
+#text examples
+divisor2_3(8)
+divisor2_3(2)
+divisor2_3(13)
