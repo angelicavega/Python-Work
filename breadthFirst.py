4661b4a5432ffe51f6404db575b81e8196e5c7ab
Angélica M. Vega Aponte <31680125+angelicavega@users.noreply.github.com>
2020-08-09 20:09:26 -0400

    Add files via upload

---
+++ breadthFirst.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def breadthFirst(startingNode, goalValue):
+  visitedNodes = set()
+  queue = deque([startingNode])
+  
+  while len(queue) > 0:
+    node = queue.pop()
+    if node in visitedNodes:
+      continue
+      
+    visitedNodes.add(node)
+    if node.value == goalValue:
+      return True
+    
+    for n in node.adjacentNodes:
+      if n not in visitedNodes:
+        queue.appendleft(n)
+        
+  return False
+
+  breadthFirst(initNode, "G")
+
+  from collections import deque
+
+def breadthFirstGrid(start)
+  visited = set()
+  frontier = deque([start])
+  
+  while len(frontier) > 0:
+    nextSquare = frontier.pop()
+    if nextSquare in visited:
+      continue
+      
+    visited.add(nextSquare)
+    if nextSquare.value == "G":
+      return True
+    
+    (x, y) = nextSquare
+
+breadthFirst()
+breadthFirstGrid()
+    
